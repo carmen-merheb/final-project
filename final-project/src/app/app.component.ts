2b4e426e9320472ac476a6f731f25490af82cc52
carmen-merheb <carmen.merheb@lau.edu>
2025-03-01 13:48:01 +0200

    integrated footer

--- final-project/src/app/app.component.ts
+++ final-project/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { LoginComponent } from './core/auth/components/login/login.component';
 import { SignupComponent } from './core/auth/components/signup/signup.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LoginComponent, SignupComponent, RouterOutlet],
+  imports: [LoginComponent, SignupComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
